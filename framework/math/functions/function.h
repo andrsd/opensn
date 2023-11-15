58f7ca9aed1facec9811f0a9471cbf59e3e3e19c
David Andrs <andrsd@gmail.com>
2023-11-15 07:14:53 -0700

    Adding function 'system'

---
+++ framework/math/functions/function.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "framework/object.h"
+
+namespace opensn
+{
+
+/**
+ * Base class for functions
+ *
+ */
+class Function : public Object
+{
+public:
+  static InputParameters GetInputParameters();
+
+protected:
+  explicit Function(const InputParameters& params);
+};
+
+typedef std::shared_ptr<Function> FunctionPtr;
+
+} // namespace opensn
