59dd74c50930b130e449672fbd8f4c7b833da35e
David Andrs <andrsd@gmail.com>
2023-11-15 18:01:36 -0700

    Introducing global namespace opensn

--- framework/mesh/field_function_interpolation/ffinterpolation.h
+++ framework/mesh/field_function_interpolation/ffinterpolation.h
@@ -3,14 +3,11 @@
 #include <memory>
 #include <vector>
 
-namespace chi_physics
+namespace opensn
 {
 class FieldFunctionGridBased;
 typedef std::shared_ptr<FieldFunctionGridBased> FieldFunctionGridBasedPtr;
-} // namespace chi_physics
 
-namespace chi_mesh::ff_interpolation
-{
 enum class Type : int
 {
   SLICE = 1,
@@ -47,29 +44,22 @@ enum class Property : int
   NUMBEROFPOINTS = 13,
   CUSTOM_ARRAY = 14,
 };
-} // namespace chi_mesh::ff_interpolation
-
-namespace chi_mesh
-{
 
 // ###################################################################
 /** Base class for field-function interpolation objects.*/
 class FieldFunctionInterpolation
 {
 protected:
-  ff_interpolation::Type type_;
+  Type type_;
   unsigned int ref_component_ = 0;
-  std::vector<chi_physics::FieldFunctionGridBasedPtr> field_functions_;
+  std::vector<FieldFunctionGridBasedPtr> field_functions_;
 
 public:
-  explicit FieldFunctionInterpolation(ff_interpolation::Type type) : type_(type) {}
+  explicit FieldFunctionInterpolation(Type type) : type_(type) {}
 
-  std::vector<chi_physics::FieldFunctionGridBasedPtr>& GetFieldFunctions()
-  {
-    return field_functions_;
-  }
+  std::vector<FieldFunctionGridBasedPtr>& GetFieldFunctions() { return field_functions_; }
 
-  ff_interpolation::Type Type() const { return type_; }
+  Type Type() const { return type_; }
 
   /**Initializes the point interpolator.*/
   virtual void Initialize(){};
@@ -80,4 +70,4 @@ public:
   virtual void ExportPython(std::string base_name) = 0;
 };
 
-} // namespace chi_mesh
+} // namespace opensn
