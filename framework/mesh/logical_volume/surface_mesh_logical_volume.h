2b8ec26689e4fe0888c84affabfe28298ed9859e
David Andrs <andrsd@gmail.com>
2023-11-23 07:59:40 -0700

    Removing SurfaceMeshPtr

--- framework/mesh/logical_volume/surface_mesh_logical_volume.h
+++ framework/mesh/logical_volume/surface_mesh_logical_volume.h
@@ -15,8 +15,8 @@ public:
   bool Inside(const Vector3& point) const override;
 
 private:
-  typedef std::shared_ptr<const SurfaceMesh> SurfaceMeshPtr;
-  const SurfaceMeshPtr surf_mesh = nullptr;
+  typedef std::shared_ptr<const SurfaceMesh> std::shared_ptr<SurfaceMesh>;
+  const std::shared_ptr<SurfaceMesh> surf_mesh = nullptr;
   std::array<double, 2> xbounds_;
   std::array<double, 2> ybounds_;
   std::array<double, 2> zbounds_;
