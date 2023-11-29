e0e595e54f820d5b5003849dbad09b22cf9106c0
David Andrs <andrsd@gmail.com>
2023-11-29 10:41:09 -0700

    Renaming chi_location_id -> location_id

--- test/modules/linear_boltzmann_solvers/transport_adjoint/adjoint_2d_1c_response.lua
+++ test/modules/linear_boltzmann_solvers/transport_adjoint/adjoint_2d_1c_response.lua
@@ -152,6 +152,6 @@ end
 
 --############################################### Cleanup
 chiMPIBarrier()
-if (chi_location_id == 0) then
+if (location_id == 0) then
     os.execute("rm Adjoint2D_1b_adjoint*.data")
 end
