99c0db7acbafce89ae9b19f95881c030600ce05d
David Andrs <andrsd@gmail.com>
2023-12-13 11:22:29 -0700

    Converting KBAGraphPartitioner test into a unit test

--- test/unit_tests/kba_graph_partitioner_test.cc
+++ test/unit_tests/kba_graph_partitioner_test.cc
@@ -1,28 +1,13 @@
-#include "lua/framework/console/console.h"
+#include "gmock/gmock.h"
 #include "framework/graphs/kba_graph_partitioner.h"
-#include "framework/object_factory.h"
-
 #include "framework/mesh/mesh.h"
 
-#include "framework/runtime.h"
-#include "framework/logging/log.h"
-
 using namespace opensn;
+using namespace testing;
 
-namespace unit_tests
+TEST(KBAGraphPartitioner, Partition)
 {
-
-ParameterBlock TestKBAGraphPartitioner00(const InputParameters&);
-
-RegisterWrapperFunctionNamespace(unit_tests,
-                                 TestKBAGraphPartitioner00,
-                                 nullptr,
-                                 TestKBAGraphPartitioner00);
-
-ParameterBlock
-TestKBAGraphPartitioner00(const InputParameters&)
-{
-  opensn::log.Log() << "GOLD_BEGIN";
+  mpi::Environment env;
 
   ParameterBlock input_parameters;
 
@@ -51,13 +36,5 @@ TestKBAGraphPartitioner00(const InputParameters&)
                                     {1.0, 1.0, 1.0}};
 
   auto cell_pids = partitioner.Partition(dummy_graph, centroids, 2 * 2 * 2);
-
-  for (const int64_t pid : cell_pids)
-    opensn::log.Log() << pid;
-
-  opensn::log.Log() << "GOLD_END";
-
-  return ParameterBlock();
+  EXPECT_THAT(cell_pids, ElementsAre(0, 1, 2, 3, 4, 5, 6, 7));
 }
-
-} //  namespace unit_tests
